aac5b621d8a6f3d768632cc0d6d9e7bcaaec7252
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-05-16 12:54:12 -0600

    FEAT: Configure JAX-RS application path (updated class name)
    This commit configures the base URI for all JAX-RS resources in the application. The `@ApplicationPath("ws")` annotation on the `JAXRSConfiguration` class (which extends `Application`) specifies that all RESTful endpoints defined in the application will be accessible under the `/ws` path.

---
+++ src/main/java/cr/ac/una/wscineuna/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package cr.ac.una.wscineuna;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Cristhofer
+ */
+@ApplicationPath("ws")
+public class JAXRSConfiguration extends Application {
+    
+}
